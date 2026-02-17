af80f1daa7bff100a5686cb9c890fc73352b27e3
0strezz <juan.dpmz05@gmail.com>
2026-02-17 09:00:00 +0000

    basic arch added

---
+++ src/test/java/edu/eci/dosw/techcup/TechcupApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.techcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
